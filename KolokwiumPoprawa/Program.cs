6468a04fc4c660fad72d20e1212314a342837b8f
Marcin Banaś <mb056625@student.ath.edu.pl>
2020-12-19 10:32:15 +0100

    Dodaj pliki projektów.

---
+++ KolokwiumPoprawa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KolokwiumPoprawa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
