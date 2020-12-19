1725bd750e112f4d05a9851277de60a2b16b0518
Marcin Banaś <mb056625@student.ath.edu.pl>
2020-12-19 11:33:19 +0100

    zmiana

---
+++ KolokwiumPoprawa/Lek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KolokwiumPoprawa
+{
+    class Lek
+    {
+        public bool OTC { get; }
+        public static string Nazwa { get; set; }
+        public static double CenaPodstawowa { get; set; }
+        public static double Promocja { get; set; }
+
+        public Lek(bool otc,string nazwa,double cenapodstawowa,double promocja)
+        {
+            OTC = otc;
+            Nazwa = nazwa;
+            CenaPodstawowa = cenapodstawowa;
+            Promocja = promocja;
+        }
+        public static double Cena()
+        {
+            return CenaPodstawowa * Promocja;
+        }
+        public static void Sprzedaj(Lek obj)
+        {
+            if (obj.OTC == true)
+            {
+                Console.WriteLine("Podaj Numer recepty");
+                string a = Console.ReadLine();
+                if (Convert.ToString(a).Length == 10)
+                {
+                    Console.WriteLine(Nazwa,Cena());
+                }
+                else
+                {
+                    throw new ArgumentException("Nr recepty jest za krotki");
+                }
+            }
+            
+        }
+    }
+}
--- KolokwiumPoprawa/Program.cs
+++ KolokwiumPoprawa/Program.cs
@@ -4,9 +4,25 @@ namespace KolokwiumPoprawa
 {
     class Program
     {
+        public static void func(ref int pkt, string odp)
+        {
+            pkt++;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a;
+            int punkty=0;
+            do
+            {
+                a = Console.ReadLine();
+                if ((String.Compare(a, "tak")>0)) ;
+                {
+                    func(ref punkty,a);
+                }
+            } while (punkty == 3);
+
+            Lek Apap = new Lek(true, "Apap", 10, 0.2);
+
         }
     }
 }
